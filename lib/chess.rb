6abb922bac8abdc13cec80ac7ad422050e117ce5
ashleymichal <ashleymichal@gmail.com>
2016-03-31 07:53:36 -0500

    Adds Player class (pulled from ConnectFour)

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,5 +1,6 @@
 require "chess/version"
 require_relative "chess/cell.rb"
+require_relative "chess/player.rb"
 
 module Chess
   # Your code goes here...
---
+++ lib/chess/player.rb
@@ -0,0 +1,9 @@
+module Chess
+  class Player
+    attr_reader :name, :color
+    def initialize(input)
+      @color = input.fetch(:color)
+      @name = input.fetch(:name)
+    end
+  end
+end
