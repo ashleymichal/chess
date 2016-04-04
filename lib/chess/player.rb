05b8dbb1cd455dab5c2f5bf9cc58da684defd05b
ashleymichal <ashleymichal@gmail.com>
2016-04-04 15:56:36 -0500

    Adds 'taken' array to track taken pieces

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -1,9 +1,15 @@
 module Chess
   class Player
+    attr_writer :taken
     attr_reader :name, :color
     def initialize(input)
       @color = input.fetch(:color)
       @name = input.fetch(:name)
+      @taken = []
+    end
+
+    def taken
+      @taken.compact
     end
   end
 end
